8ed5341572a42dfc13b6d3aa814b3aaf78525b28
Prasant Kumar <prasant.kumar-ext@guavus.com>
2018-10-08 16:52:03 +0530

    adding test stage

--- jenkins/jobs/pipeline/demo_app.groovy
+++ jenkins/jobs/pipeline/demo_app.groovy
@@ -7,11 +7,28 @@
           url: 'https://github.com/prasantkumar-ext/devops.git'
 
 
-          stage('Build') {
+          stage('Gradle Build') {
 
                sh"""
-               echo "Hi This is Demo Stage"
+               ./gradlew clean build --refresh-dependencies
                """
-	  }
+	      }
+
+	      stage('Gradle Test') {
+               sh"""
+                 ./gradlew check && \
+                 ./gradlew jacocoTestReport && \
+                 ./gradlew sonarqube
+               """
+          }
+          stage('Publish TestReport') {
+               junit 'build/test-results/test/*.xml'
+               publishHTML([allowMissing: false,
+                 alwaysLinkToLastBuild: false,
+                 keepAll: false,
+                 reportDir: 'build/reports/tests/test/',
+                 reportFiles: 'index.html',
+                 reportName: 'Coverage Report'])
+          }
 
 }
