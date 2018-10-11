40af4be756bbfb4cbaa44087b005e0efa5f2aac3
Prasant Kumar <prasant.kumar-ext@guavus.com>
2018-10-11 18:23:59 +0530

    adding dp

--- jenkins/jobs/pipeline/ng_collector.groovy
+++ jenkins/jobs/pipeline/ng_collector.groovy
@@ -1,12 +1,18 @@
   node {
 
           git branch: env.GIT_BRANCH,
-
           credentialsId: 'github',
-
           url: 'https://github.com/prasantkumar-ext/ng-collector.git'
 
-          currentBuild.description = "BranchName: ${env.GIT_BRANCH}"
+          stage('Add Job Description') {
+
+              def userEmail = sh(
+                            script: 'git log -1 --pretty=format:"UserName: %ae"',
+                            returnStdout: true)
+                  dp = userEmail + " Branch: ${env.GIT_BRANCH}"
+              currentBuild.description = dp
+          }
+
           stage('Gradle Build') {
 
                sh"""
@@ -14,5 +20,4 @@
                ./build.sh
                """
 	      }
-
 }
